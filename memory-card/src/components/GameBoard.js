25eaa0d9beef78f21c8b1af6649e0d6302aa0b72
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-10-19 19:31:48 -0400

    Created GameBoard Component.
    Created GameBoard Component.

---
+++ memory-card/src/components/GameBoard.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './GameBoard.css';
+
+export default class GameBoard extends Component {
+    render() {
+        return (
+            <div id='gameBoard'>
+                GameBoard
+            </div>
+        );
+    }
+}
--- memory-card/src/index.js
+++ memory-card/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Nav from './components/Nav';
+import GameBoard from './components/GameBoard';
 
 const content = ReactDOM.createRoot(document.getElementById('content'));
 content.render(
   <React.StrictMode>
     <Nav />
+    <GameBoard />
   </React.StrictMode>
 );
